c5f890dbedc3aab0319cffe43a8378112788f6e2
Wiji <peawseaw@gmail.com>
2022-02-09 13:41:43 -0800

    commit

--- OOP/Zoo Keeper Part 2/src/com/wiji/zookeeper2/Bat.java
+++ OOP/Zoo Keeper Part 2/src/com/wiji/zookeeper2/Bat.java
@@ -1,5 +1,26 @@
 package com.wiji.zookeeper2;
 
 public class Bat extends Mammal {
-
+	
+	public Bat() {
+		super();
+		super.setEnergyLevel(300);
+	}
+	
+	public void fly() {
+		super.setEnergyLevel(super.getEnergyLevel()-10);
+		System.out.printf("hssss %d\n", super.getEnergyLevel());
+		
+	}
+	
+	public void eatHumans() {
+		super.setEnergyLevel(super.getEnergyLevel()+25);
+		System.out.printf("So-well, never mind just increase its energy to %d\n", super.getEnergyLevel());
+	}
+	
+	public void attackTown() {
+		super.setEnergyLevel(super.getEnergyLevel()-100);
+		System.out.printf("Bats attack---help!! Energy lost to %d\n", super.getEnergyLevel());
+	}
+	
 }
--- OOP/Zoo Keeper Part 2/src/com/wiji/zookeeper2/TestMammal.java
+++ OOP/Zoo Keeper Part 2/src/com/wiji/zookeeper2/TestMammal.java
@@ -2,6 +2,16 @@ package com.wiji.zookeeper2;
 
 public class TestMammal {
 	public static void main(String[] args){
+			
+		Bat B1 = new Bat();
+		B1.attackTown();
+		B1.attackTown();
+		B1.attackTown();
+		B1.eatHumans();
+		B1.eatHumans();
+		B1.fly();
+		B1.fly();
 
 	}
+
 }
