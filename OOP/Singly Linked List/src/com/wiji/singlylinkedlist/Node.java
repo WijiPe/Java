c407a7304062234891756000038efa7bd4410287
Wiji <peawseaw@gmail.com>
2022-02-13 22:33:17 -0800

    commit

---
+++ OOP/Singly Linked List/src/com/wiji/singlylinkedlist/Node.java
@@ -0,0 +1,13 @@
+package com.wiji.singlylinkedlist;
+
+public class Node {
+    public int value;
+    public Node next;
+    
+    
+    public Node(int value) {
+    	this.value = value;
+    	this.next = null;
+    }
+    
+}
---
+++ OOP/Singly Linked List/src/com/wiji/singlylinkedlist/SinglyLinkedList.java
@@ -0,0 +1,62 @@
+package com.wiji.singlylinkedlist;
+
+public class SinglyLinkedList {
+    public Node head;
+    
+    public SinglyLinkedList() {
+
+    }
+    // SLL methods go here. As a starter, we will show you how to add a node to the list.
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if(head == null) {
+            head = newNode;
+        } else {
+            Node runner = head;
+            while(runner.next != null) {
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+        
+        
+    }
+    public void remove(){
+        if(this.head != null){
+        	Node runner = head;
+        	if(runner.next == null){
+        		head=null;
+        	}
+        	while(runner.next.next != null){
+        		runner = runner.next;
+        	}
+        	runner.next = null;
+        }
+    }
+    
+    public void printValues(){
+    	if(head == null){
+            System.out.println("Emthy List");
+        }else {
+        	Node runner = head;
+        	while(runner != null){
+        		System.out.println(runner.value);
+        		runner = runner.next;
+        	}		
+        }
+    }
+    
+    public void find(int value) {
+        	Node runner = head;
+        	 if(runner.value == value) {
+        		 System.out.println(runner.value);
+        	 }
+        	 while(runner.next != null){
+        		 if(runner.next.value == value) {
+        			 System.out.println(runner.next.value);
+        		 }runner = runner.next;
+        	 }
+        }
+}
+
+
